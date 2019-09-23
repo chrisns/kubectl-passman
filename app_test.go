56889035f877cfb46468fb2e58f37aa20df65f80
Chris Nesbitt-Smith <chris@cns.me.uk>
2019-09-23 16:02:36 +0100

    stubs to get tests and ci going

---
+++ app_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_fail(t *testing.T) {
+	assert.True(t, false)
+}
+func Test_true(t *testing.T) {
+	assert.True(t, false)
+}

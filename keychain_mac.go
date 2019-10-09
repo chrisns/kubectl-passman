92b07800cde60cb153416f4a0ebd172fbd60ac6c
Chris Nesbitt-Smith <chris@cns.me.uk>
2019-10-09 14:59:11 +0100

    add set and delete functions

--- keychain_mac.go
+++ keychain_mac.go
@@ -12,7 +12,6 @@ var defaultKeychain = func(serviceLabel string) ([]keychain.QueryResult, error) 
 	query := keychain.NewItem()
 	query.SetSecClass(keychain.SecClassGenericPassword)
 	query.SetService(serviceLabel)
-	query.SetMatchLimit(keychain.MatchLimitOne)
 	query.SetReturnData(true)
 	return keychain.QueryItem(query)
 }
@@ -27,4 +26,29 @@ func keychainFetcher(serviceLabel string) string {
 	}
 	password := string(results[0].Data)
 	return password
+}
+
+func keychainWriter(serviceLabel string, secret string) {
+	item := keychain.NewItem()
+	item.SetSecClass(keychain.SecClassGenericPassword)
+	item.SetService(serviceLabel)
+	item.SetAccount(serviceLabel)
+	item.SetData([]byte(secret))
+	err := keychain.AddItem(item)
+
+	if err == keychain.ErrorDuplicateItem {
+		keychainDeleter(serviceLabel)
+		keychainWriter(serviceLabel, secret)
+	}
+}
+
+func keychainDeleter(serviceLabel string) {
+	query := keychain.NewItem()
+	query.SetSecClass(keychain.SecClassGenericPassword)
+	query.SetService(serviceLabel)
+	query.SetMatchLimit(keychain.MatchLimitOne)
+	err := keychain.DeleteItem(query)
+	if err == keychain.ErrorDuplicateItem {
+		panic("errored deleting")
+	}
 }

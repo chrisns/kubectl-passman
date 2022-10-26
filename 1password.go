87321be22d9c7bea7b0643043282c9d02928a6db
Xphyr <xphyr@users.noreply.github.com>
2022-10-26 11:26:22 -0400

    removing the need for creating a file to add credential to 1password
    Signed-off-by: Xphyr <xphyr@users.noreply.github.com>

--- 1password.go
+++ 1password.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"os"
 	"os/exec"
 )
 
@@ -56,47 +55,11 @@ func opgetter(itemName string) (string, error) {
 
 func opsetter(itemName, secret string) error {
 
-	var res = &opResponse{
-		Title:    itemName,
-		Category: "API_CREDENTIAL",
-		Fields: []opResponseField{
-			{
-				Id:    "credential",
-				Label: "credential",
-				Type:  "CONCEALED",
-				Value: secret,
-			},
-			{
-				Id:    "type",
-				Label: "type",
-				Type:  "MENU",
-				Value: "json",
-			},
-		},
-	}
+	// create a string that will be passed to the op command to store our secret
+	credString := "credential[concealed]=" + secret
 
-	jsonResponse, err := json.Marshal(res)
-
-	if err != nil {
-		return err
-	}
-
-	// the best way to write a new value to 1password seems to be using a json file and crating an entry from that
-	// we will create a temp file, and then delete it when this function returns to be as secure as possible
-
-	f, err := os.CreateTemp("", "sample")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Temp file name:", f.Name())
-	defer os.Remove(f.Name())
-	_, err = f.Write(jsonResponse)
-	if err != nil {
-		panic(err)
-	}
-	f.Close()
-
-	stdoutStderr, err := exec.Command("op", "item", "create", "--template", f.Name()).CombinedOutput()
+	stdoutStderr, err := exec.Command("op", "item", "create", "--category",
+		"API Credential", "--title", itemName, credString).CombinedOutput()
 
 	if err != nil {
 		fmt.Printf("%s\n", stdoutStderr)
